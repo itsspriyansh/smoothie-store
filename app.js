4766dec476f6366ce7a1a699a3b36b1ded02cf9e
itsspriyansh <priyanshkp01@gmail.com>
2023-03-03 15:48:53 +0530

    added server file

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express = require('express');
 const connectDb = require('./config/connection');
 
 const app = express();
-const PORT = process.env.PORT || 3000
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-if (connectDb()) app.listen(PORT, () => console.log(`server listening on port number : ${PORT}`))
+connectDb()
 
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
+
+module.exports = app
