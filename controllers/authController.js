092640b2a2cf2067748faf2ea7e3069ac10f8618
itsspriyansh <priyanshkp01@gmail.com>
2023-03-04 18:04:47 +0530

    added jwt cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,9 @@
 const User = require("../models/User")
+const jwt = require("jsonwebtoken")
+const cookie = require("cookie-parser")
 
 const handleError = (error) => {
-
     let errors = { email : "", password : "" }
-
     if (error.message.includes("user validation failed")) {
         Object.values(error.errors).forEach(({ properties }) => {
             errors[properties.path] = properties.message
@@ -14,6 +14,14 @@ const handleError = (error) => {
     }
     return errors
 }
+
+const maxAge = 3 * 24 * 60 * 60
+const createToken = (id) => {
+    return jwt.sign({ id }, "NoOneCanHoldTheirBreathForever", {
+        expiresIn : maxAge
+    })
+}
+
 
 module.exports.login_get = (req, res) => {
     res.render("login")
@@ -31,7 +39,9 @@ module.exports.signup_post = async (req, res) => {
     const { email, password } = req.body
     try {
         const result = await User.create({ email, password })
-        res.status(201).json(result)
+        const token = createToken(result._id)
+        res.cookie("jwt", token, { httpOnly : true, maxAge : maxAge * 1000 })
+        res.status(201).json({ result : result._id})
     } catch (error) {
         const err = handleError(error)
         res.status(400).json(err)
