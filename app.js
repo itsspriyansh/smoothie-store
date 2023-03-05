033f510673722f51a526d7dedb70982c66ccbc21
itsspriyansh <priyanshkp01@gmail.com>
2023-03-05 10:52:41 +0530

    added auth middleware

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const connectDb = require('./config/connection');
 const authRoutes = require("./routes/authRoutes")
-const cookieParser = require("cookie-parser")
+const cookieParser = require("cookie-parser");
+const requireAuth = require('./middleware/authMiddleware');
 
 const app = express();
 
@@ -14,11 +15,11 @@ app.set('view engine', 'ejs');
 connectDb()
 
 app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
 app.use(authRoutes)
 
 app.get("/set-cookies", (req, res) => {
-    res.cookie("newUser", false)
+    res.cookie("newUser", true)
     res.cookie("isEmployee", true, {maxAge : 1000 * 10, httpOnly : true})
     res.send("cookies set!")
 })
